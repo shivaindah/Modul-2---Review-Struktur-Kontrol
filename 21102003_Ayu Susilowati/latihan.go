252aea8c0b5c659225560ec60222bac4d79ed51d
Ayu Susilowati <164893417+ayususilowatii@users.noreply.github.com>
2024-09-27 11:07:40 +0700

    Add files via upload

---
+++ 21102003_Ayu Susilowati/latihan.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var a,b,c,d,e int
+	var hasil int
+	fmt. Scanln(&a, &b, &c, &d, &e)
+
+	hasil = a+b+c+d+e
+	fmt.Println("Hasil Penjumlahan ",a,b,c,d,e, "adalah ",hasil)
+} 
